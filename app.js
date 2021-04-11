fe848067eebf4cd6e464272dfc7a4c19b0bdcd4c
ambaqinejad <ambaqinejad@gmail.com>
2021-04-11 16:23:02 -0400

    Heroku initialization

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.json({
+        name: 'Heroku app'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+})
