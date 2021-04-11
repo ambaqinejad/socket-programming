5582cab68ebcc5a17d89087dabcf840c94dd23e6
ambaqinejad <ambaqinejad@gmail.com>
2021-04-11 16:56:12 -0400

    adding public folder

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
     res.json({
